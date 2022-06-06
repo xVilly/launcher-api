aa9614f62316e63238871b334a674f4ed69df639
xVilly <74141765+xVilly@users.noreply.github.com>
2022-06-06 21:48:10 +0200

    init4

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 from flask import Flask
-from flask_restful import Api, Resource, reqparse
+from flask_restful import Api, Resource, reqparse, abort
 
 app = Flask(__name__)
 api = Api(app)
 
+name_arg_template = reqparse.RequestParser()
+name_arg_template.add_argument("name", type=str, help="Provide your name (string)", required=True)
+
 class HelloWorld(Resource):
-    def get(self, name):
-        return {'hello':f'{name} said hello world'}
+    def get(self, _id):
+        args = name_arg_template.parse_args()
+        return {'hello':f'{_id} said hello world', 'args':args}
 
-api.add_resource(HelloWorld, "/<int:name>")
-
-print("Hello world")
+api.add_resource(HelloWorld, "/<int:_id>")
 
 if __name__ == "__main__":
     app.run(debug=True)
